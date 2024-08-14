33b4b044baa70de199dcd0206b7b76807b1307bb
sengoku-f <31064651+sengoku-f@users.noreply.github.com>
2024-08-14 11:00:51 +0800

    修改 baseurl

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   configureWebpack: {
     resolve: {
       alias: {
